7fda64a26f7e784727cae3a6492e71ee2dffd3e6
Jm <matrixgogo123@gmail.com>
2026-02-13 16:33:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-const frames = document.querySelectorAll(".frame");
-const finalFrame = document.querySelector(".final-frame");
-let current = 0;
+const pages = document.querySelectorAll(".page");
+let currentPage = 0;
 
-function startExperience() {
-  document.querySelector(".start-screen").style.display = "none";
-  document.querySelector(".container").classList.remove("hidden");
-
-  const music = document.getElementById("bgMusic");
-  music.play();
-
-  startFrames();
-}
-
-function startFrames() {
-  setInterval(() => {
-    if (current < frames.length) {
-      frames[current].classList.remove("active");
-      current++;
-      if (current < frames.length) {
-        frames[current].classList.add("active");
-      } else {
-        finalFrame.classList.add("active");
-      }
-    }
-  }, 2500);
+function nextPage() {
+  pages[currentPage].classList.remove("active");
+  currentPage++;
+  if (currentPage < pages.length) {
+    pages[currentPage].classList.add("active");
+  }
 }
